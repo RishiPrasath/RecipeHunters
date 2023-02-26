950ecb5147967fc4a0c23e8ceb5a1afdfe175e7d
RishiPrasath <73206126+RishiPrasath@users.noreply.github.com>
2023-02-26 22:27:03 +0800

    /searchResults route
    Implemented search for the express route only. Front-end WIP

--- Back-End/routes/Search.js
+++ Back-End/routes/Search.js
@@ -1,18 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const { MongoClient } = require("mongodb");
 
+
+const bodyParser = require('body-parser')
+
+
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: false }))
 
 router.get('/',(req,res)=>{
     res.send("Search Route");
 
 })  
 
-router.get('/searchResults',(req,res)=>{
-console.log('/searchResults');
+router.get('/searchResults/:query', async (req,res)=>{
+    console.log('/searchResults');
 //Database Query Requirements
 //Filter Recipes by query name
 //Filter Recipes by filters using the tags field in the Recipe Document 
-
+    console.log(req.params.query);
+    const query =  req.params.query;
+    const client = await MongoClient.connect(
+        'mongodb+srv://RecipeHunters:t4g5@cluster0.evmmugl.mongodb.net/test',
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    const coll = client.db('RecipeHunters').collection('Recipes');
+    const pipeline = [{
+        '$search': {
+          'index': 'recipesearch',
+          'text': { 'query': query,'path': {'wildcard': '*'},'fuzzy': {}}
+        }
+    }];
+    const cursor = coll
+    .aggregate(pipeline).project({name:1,imageURLs:1,summary_tags:1,videoURL:1,briefdescription:1}); // Limiting fields to be displayed;
+    const result = await cursor.toArray();
+    console.log(result);
+    await client.close();
+    res.json(result);
 }) 
 
 
--- Front-End/src/Components/Search/Search.js
+++ Front-End/src/Components/Search/Search.js
@@ -3,8 +3,14 @@ import Filters from "./Child Compoenents/Filter";
 import FiltersList from "./Child Compoenents/FiltersList";
 import ResultsHeader from "./Child Compoenents/ResultsHeader"
 import RecipeElement from "../RecipeElement";
-
+import {useLocation} from "react-router-dom";
 function Search(){
+
+    const location = useLocation();
+    const pathname = decodeURI(location.pathname);
+    const searchMode = pathname.replace('/search/','');
+    console.log(searchMode);
+
     return(
         <>
             <SearchBar/>
